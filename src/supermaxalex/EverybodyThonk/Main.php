0939cbdf91f929e73c2d59ac727407c953bbbbf2
Max <marielolo93@gmail.com>
2018-08-08 10:06:53 +0200

    Use PluginLoader instead of using the server logger
    Thanks @dktapps

--- src/supermaxalex/EverybodyThonk/Main.php
+++ src/supermaxalex/EverybodyThonk/Main.php
@@ -7,18 +7,16 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener{
 
-	private const PREFIX = "[EverybodyThonk] ";
-
 	//TODO: config - make possible to change thonk head's skin, exclude a player...
 
 	public function onEnable() : void{
 		ThonkHead::init();
 
-		$this->getServer()->getLogger()->info(self::PREFIX . "Everybody will now thonk.");
+		$this->getLogger()->info("Everybody will now thonk.");
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
 	}
 
 	public function onDisable() : void{
-		$this->getServer()->getLogger()->info(self::PREFIX . "Everybody are stopping to thonk.");
+		$this->getLogger()->info("Everybody are stopping to thonk.");
 	}
 }
